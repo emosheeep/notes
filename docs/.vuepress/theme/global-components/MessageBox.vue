267b94d59f25383ced5e8c1a82a06800a76f419e
QinXuYang <1446445040@qq.com>
2020-04-19 14:55:40 +0800

    解决valine评论不刷新的问题

--- docs/.vuepress/theme/global-components/MessageBox.vue
+++ docs/.vuepress/theme/global-components/MessageBox.vue
@@ -3,23 +3,19 @@
 </template>
 
 <script>
+import Valine from 'valine'
 export default {
   name: 'MessageBox',
   mounted () {
-    import(
-      /* webpackChunkName: "valine" */
-      'valine'
-    ).then(({ default: Valine }) => {
-      // eslint-disable-next-line no-new,no-undef
-      new Valine({
-        el: '#vcomments',
-        appId: '2qJAldnFouTSw9I36vfaQVFV-gzGzoHsz',
-        appKey: 'N333N5NmrdALyAFsdtQ7g3XN',
-        avatar: 'retro',
-        placeholder: '填写邮箱可以收到回复哦~',
-        verify: true, // 验证码
-        recordIP: true // 记录ip
-      })
+    // eslint-disable-next-line no-new,no-undef
+    new Valine({
+      el: '#vcomments',
+      appId: '2qJAldnFouTSw9I36vfaQVFV-gzGzoHsz',
+      appKey: 'N333N5NmrdALyAFsdtQ7g3XN',
+      avatar: 'retro',
+      placeholder: '填写邮箱可以收到回复哦~',
+      recordIP: true, // 记录ip
+      path: location.pathname
     })
   }
 }
